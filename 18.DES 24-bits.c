fe3b88ed2bd3ee88c97708930f8fbc2d59a9efc9
Gunavarshavg <112234654+Gunavarshavg@users.noreply.github.com>
2023-11-22 12:30:36 +0530

    Update 18.DES 24-bits.c

--- 18.DES 24-bits.c
+++ 18.DES 24-bits.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
-
-// Key permutation table
+ 
 int keyPermutationTable[] = {
     57, 49, 41, 33, 25, 17, 9, 1,
     58, 50, 42, 34, 26, 18, 10, 2,
@@ -10,33 +9,22 @@ int keyPermutationTable[] = {
     31, 23, 15, 7, 62, 54, 46, 38,
     30, 22, 14, 6, 61, 53, 45, 37,
     29, 21, 13, 5, 28, 20, 12, 4
-};
-
-// Circular left shift schedule
+}; 
 int shiftSchedule[] = {
     1, 1, 2, 2, 2, 2, 2, 2,
     1, 2, 2, 2, 2, 2, 2, 1
 };
-
-// Key generation function
 void generateSubkeys(uint64_t initialKey, uint64_t subkeys[16]) {
-    // Perform initial permutation on the key
+     
     uint64_t permutedKey = 0;
     for (int i = 0; i < 56; ++i) {
         permutedKey |= ((initialKey >> (64 - keyPermutationTable[i])) & 0x01) << (55 - i);
-    }
-
-    // Split the 56-bit key into two 28-bit halves
+    } 
     uint32_t leftHalf = (uint32_t)(permutedKey >> 28) & 0x0FFFFFFF;
-    uint32_t rightHalf = (uint32_t)(permutedKey & 0x0FFFFFFF);
-
-    // Generate 16 subkeys
-    for (int round = 0; round < 16; ++round) {
-        // Perform circular left shifts based on the schedule
+    uint32_t rightHalf = (uint32_t)(permutedKey & 0x0FFFFFFF); 
+    for (int round = 0; round < 16; ++round) { 
         leftHalf = ((leftHalf << shiftSchedule[round]) | (leftHalf >> (28 - shiftSchedule[round]))) & 0x0FFFFFFF;
-        rightHalf = ((rightHalf << shiftSchedule[round]) | (rightHalf >> (28 - shiftSchedule[round]))) & 0x0FFFFFFF;
-
-        // Combine the two halves and perform the subkey permutation
+        rightHalf = ((rightHalf << shiftSchedule[round]) | (rightHalf >> (28 - shiftSchedule[round]))) & 0x0FFFFFFF; 
         uint64_t combinedHalf = ((uint64_t)leftHalf << 28) | rightHalf;
         subkeys[round] = 0;
         for (int i = 0; i < 48; ++i) {
@@ -45,17 +33,10 @@ void generateSubkeys(uint64_t initialKey, uint64_t subkeys[16]) {
     }
 }
 
-int main() {
-    // Example initial key
-    uint64_t initialKey = 0x133457799BBCDFF1;
-
-    // Array to store the 16 subkeys
-    uint64_t subkeys[16];
-
-    // Generate subkeys
-    generateSubkeys(initialKey, subkeys);
-
-    // Display the subkeys
+int main() { 
+    uint64_t initialKey = 0x133457799BBCDFF1; 
+    uint64_t subkeys[16]; 
+    generateSubkeys(initialKey, subkeys); 
     for (int i = 0; i < 16; ++i) {
         printf("Subkey %2d: %012llx\n", i + 1, subkeys[i]);
     }
